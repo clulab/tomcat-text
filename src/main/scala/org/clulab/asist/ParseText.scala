737cf9fd026ef86f24533df7d52b84c11d2ac16d
Peter Lovett <pelovett@gmail.com>
2020-02-25 12:56:41 -0700

    Reorganizes initial grammars

--- src/main/scala/org/clulab/asist/ParseText.scala
+++ src/main/scala/org/clulab/asist/ParseText.scala
@@ -1,14 +1,23 @@
 package org.clulab.asist
 
+import org.clulab.asist.AsistEngine
 import org.clulab.processors.Processor
 import org.clulab.processors.fastnlp.FastNLPProcessor
 
 object ParseText extends App {
 
-  val a = "Hello world"
-  println(a)
+    val text = "I attack the zombie"
+    println(s"Processing sentence : ${text}" )
+ 
+    println("[AsistEngine] Initializing the AsistEngine ...")
+    val ieSystem = new AsistEngine()
+    var proc = ieSystem.proc
+    println("[AsistEngine] Completed Initialization ...") 
+    val doc = ieSystem.annotate(text)
 
-
-
-
+    // extract mentions from annotated document
+    val mentions = ieSystem.extractFrom(doc).sortBy(m => (m.sentence, m.getClass.getSimpleName))
+    println(s"Labels: ${mentions.map(m => m.label).mkString(",\t")}")
+    
+  
 }
