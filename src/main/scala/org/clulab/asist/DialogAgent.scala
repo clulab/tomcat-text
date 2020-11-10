22be043b5ab76412640e91a9eda475d2aa8d1a30
Joseph Astier <jastier@email.arizona.edu>
2020-11-10 12:44:35 -0700

    added stub file for new DialogAgent

---
+++ src/main/scala/org/clulab/asist/DialogAgent.scala
@@ -0,0 +1,21 @@
+package org.clulab.asist
+
+import java.io.{File, PrintWriter}
+import java.util.Properties
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP
+import spray.json._
+import DefaultJsonProtocol._
+
+import scala.collection.{immutable, mutable}
+import scala.io.Source
+import scala.util.parsing.json.JSON
+
+object DialogInfo extends App {
+
+  println("DialogInfo Starting")
+
+  // ...
+
+  println("DialogInfo Ending")
+}
