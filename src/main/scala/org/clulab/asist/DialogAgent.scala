bf1fa7f754d202b91e38a0fe879a504d9a98eecc
Joseph Astier <jastier@gmail.com>
2020-11-17 20:00:19 -0700

    Removed imports to become Mosquitto agnostic

--- src/main/scala/org/clulab/asist/DialogAgent.scala
+++ src/main/scala/org/clulab/asist/DialogAgent.scala
@@ -1,12 +1,5 @@
 package org.clulab.asist
 
-import org.eclipse.paho.client.mqttv3.MqttClient
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions
-import org.eclipse.paho.client.mqttv3.MqttException
-import org.eclipse.paho.client.mqttv3.MqttMessage
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
-
-
 
 // coordinator for the Mosquitto subscriber and publisher
 
