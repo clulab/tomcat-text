1e2eb45dfa04841edae7beddf02ef99a814772f3
Joseph Astier <jastier@gmail.com>
2020-11-17 17:26:49 -0700

    Added Mosquitto publisher

---
+++ src/main/scala/org/clulab/asist/MqttPublish.scala
@@ -0,0 +1,55 @@
+package org.clulab.asist
+
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttException
+import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+
+/**
+ * A singleton class to provide a convenient interface to the Eclipse Paho
+ * MQTT client library.
+ */
+
+object MqttPublish extends App {
+
+    println("MqttPublish Starting")
+
+    val broker = "tcp://127.0.0.1:1883"
+    val text = "Hello Mosquitto!"
+    val qos = 2
+    val clientID = "TomcatClient"
+    val topic = "example_topic"
+    val persistence = new MemoryPersistence()
+
+    try {
+        val client = new MqttClient(broker, clientID, persistence)
+        val connectOptions = new MqttConnectOptions()
+        connectOptions.setCleanSession(true)
+        println("Connecting to broker: "+broker)
+        client.connect(connectOptions)
+        println("Connected")
+        println("On topic: "+topic)
+        println("Publishing message: "+text)
+        val message = new MqttMessage(text.getBytes())
+        message.setQos(qos)
+        client.publish(topic, message)
+        println("Message published")
+        client.disconnect()
+        println("Disconnected")
+    }
+    catch {
+        case me: MqttException => {
+            println("MqttException")
+        }
+    }
+    
+
+    println("MqttPublish Stopping")
+}
+
+
