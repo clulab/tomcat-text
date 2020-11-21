676d499dd5936556e3fdf1123317615ad51d5d34
Joseph Astier <jastier@gmail.com>
2020-11-21 00:06:34 -0700

    Removed early dev files no longer needed

--- src/main/scala/org/clulab/asist/DialogAgent.scala
+++ src/main/scala/org/clulab/asist/DialogAgent.scala
@@ -1,49 +1,36 @@
-//  DialogAgent
+//  DialogRelayer
 //
-//  Author:  Joseph Astier, Adarsh Pyarelal
+//  Author:  Joseph Astier
 //  Date:  2020 November
 //
-//  MQTT message relayer, subscribes to messages on one topic and
-//  then re-publishes them on another.
-//  Based on the Eclipse paho MQTT package defined here:
+//  Asynchronous event-driven Mosquitto message subscriber
+//  Based on the Eclipse Paho Mosquitto package:
 //  https://www.eclipse.org/paho/files/javadoc/org/eclipse/paho/client/mqttv3/package-tree.html
 //
 package org.clulab.asist
 
-
-// coordinator for the Mosquitto subscriber and publisher
 import org.eclipse.paho.client.mqttv3._
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
+class DialogRelayer(
+        clientId: String, 
+        brokerUri: String, 
+        srcTopic: String,
+        dstTopic: String,
+        qos: Int) 
+            extends DialogSubscriber(clientId + "Sub", brokerUri, qos) {
 
-object DialogAgent extends App {
+    val publisher = new DialogPublisher(clientId + "Pub",  brokerUri, qos) 
 
-    val brokerUri   = "tcp://127.0.0.1:1883"
-    val qos         = 2
-    val topic       = "example_topic"
-    val relay_topic = "relay_example_topic"
-    val message     = "Hello Mosquitto!"
+    override def connect() : Unit = {
+        super.connect()
+        publisher.connect()
+        subscribe(srcTopic)
+    }
 
-    val subscriber_to_topic = new DialogSubscriber("Sub1",  brokerUri, qos)
-    val subscriber_to_relay_topic = new DialogSubscriber("Sub2",  brokerUri, qos)
-    val publisher   = new DialogPublisher ("Pub",   brokerUri, qos)
-
-    subscriber_to_topic.connect()
-    subscriber_to_relay_topic.connect()
-    publisher.connect()
-
-    subscriber_to_topic.subscribe(topic)
-    subscriber_to_relay_topic.subscribe(relay_topic)
-
-    // proof-of-concept test:   
-
-    // Publishing on topic will reach subscriber_to_topic
-    publisher.publish(topic, message) 
-
-    // add a relayer to send topic messages to relay_topic
-    val relay = new DialogRelayer ("Relay", brokerUri, topic, relay_topic, qos)
-    relay.connect()
-
-    // Publishing on topic will now reach subscriber_to_topic AND subscriber_to_relay_topic
-    publisher.publish(topic, message) 
+    override def messageArrived(topic: String, msg: MqttMessage): Unit = { 
+        publisher.publish(dstTopic, msg.toString)
+    }
 }
+
+
