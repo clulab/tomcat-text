4678ae55289fa561677a719fe77905001bd6bbe9
Joseph Astier <jastier@gmail.com>
2021-01-13 17:11:45 -0700

    Cleaned up DialogAgent exception handling

--- src/main/scala/org/clulab/asist/DialogAgent.scala
+++ src/main/scala/org/clulab/asist/DialogAgent.scala
@@ -7,177 +7,126 @@
 //
 package org.clulab.asist
 
-//import java.io.{File, PrintWriter}
-//import org.clulab.odin.Mention
-
-import edu.stanford.nlp.pipeline.StanfordCoreNLP
-import java.util.{Arrays, Properties}
-import org.clulab.processors.Document
+import java.net.ConnectException
 import org.eclipse.paho.client.mqttv3._
-
-import org.json4s._
-import scala.collection.mutable.ArrayBuffer
-import scala.collection.immutable
-import scala.io.Source
-import scala.util.parsing.json.JSON
-import spray.json._
-import spray.json.DefaultJsonProtocol._
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
-import org.json4s.jackson.JsonMethods.{compact, parse, render}
 
+// this object allows a test of the DialogAgent
+object DialogAgentTest extends App {
+
+  val host = "127.0.0.1"
+  val port = 1883
+  val agent = new DialogAgent(host, port)
+
+  // Send a message on the relay source topic.   It should be relayed to
+  // the relay destination topic.
+  agent.publish(agent.relaySrc, "Relay this message")
+
+  // Send a message on the relay destination topic.  It should not be relayed.
+  agent.publish(agent.relayDst, "Do not relay this message")
+}
 
 
 //  Asynchronous event-driven MQTT agent. Messages read on one
-//  bus topic are published on another
-class DialogAgent(val host: String, val port: Int) extends MqttCallback {
-  val id = "DialogAgent"  // arbitrary name for MQTT broker connection
-  val subId = "DialogAgentSubscriber" // arbitrary, just has to be unique
-  val pubId = "DialogAgentPublisher" // arbitrary, just has to be unique
-  val uri = "tcp://%s:%d".format(host, port)  // location of MQTT broker
+//  bus topic are resent on another
+class DialogAgent(host: String, port: Int) extends MqttCallback {
+  val id = "DialogAgent"
+  val uri = "tcp://%s:%d".format(host, port)
   val relaySrc = "observations/chat" // relay messages from here
   val relayDst = "agent/tomcat_chatbot" // relay messages to here
-  val qos = 2  // MQTT quality of service, 2 is highest
+  val qos = 2
   val verbose = true // set true for debug printf output
-  val subscriber = new MqttAsyncClient(uri, subId, new MemoryPersistence)
-  val publisher = new MqttClient(uri, pubId, new MemoryPersistence)
+  val subscriber = connectSubscriber("DialogAgentSubscriber")
+  val publisher = connectPublisher("DialogAgentPublisher")
 
-  // Build an extractor for our tokens
-  val pipeline = new StanfordCoreNLP(new Properties() {
-    setProperty(
-      "annotators",
-      "tokenize, ssplit, pos, lemma, ner, parse, dcoref"
-    )
-  })
-  val json_tax_map = JsonParser(
-    Source.fromResource("taxonomy_map.json").mkString
-  ).convertTo[immutable.Map[String, Array[immutable.Map[String, String]]]]
-  val extractor = new Extractor(pipeline, new AsistEngine(), json_tax_map)
+  // print status reports if the 'verbose' flag is set.
+  private def report(msg: String): Unit = 
+    if (verbose) println("%s: %s".format(id,msg))
 
-  // init tasks
-  subscriber.setCallback(this)
+  report("Topic %s will be relayed to %s".format(relaySrc, relayDst))
 
-  // output status updates to stdout if the verbose flag is true
-  def log(msg: String): Unit = if (verbose) {
-    println("%s: %s".format(id, msg))
-  }
-
-  // dump an extractions to text
-  def sayExtractions(extractions: ArrayBuffer[Array[Any]]): Unit = { 
-    log("Extractions:")
-    extractions.foreach(extraction => {
-      log("  Extraction:")
-      extraction.foreach(e => log("    '%s'".format(e.toString)))
-    })
-  }
-
-
-  // convert clulab.processor.Document to JSON
-  def toJson(extractions: ArrayBuffer[Array[Any]], doc: Document): String = {
-    sayExtractions(extractions)
-    "Extractions: %s".format(extractions.toString)
-  }
-
-  // relay a message
-  def relay(msg: MqttMessage): Unit = {
-    val text = msg.toString
-    log("Relaying '%s' from [%s] to [%s]".format(text, relaySrc, relayDst))
-    val (extractions, doc) = extractor.runExtraction(text, "")
-    publish(relayDst, toJson(extractions, doc))
-  }
-
-  // Publish a string
-  def publish(topic: String, text: String): Boolean =
-    publish(topic, text.getBytes)
-
-  // Publish any byte array
-  def publish(topic: String, payload: Array[Byte]): Boolean =
-    publish(topic, new MqttMessage(payload))
-
-  // Publish message
-  def publish(topic: String, msg: MqttMessage): Boolean = try {
-    if (publisher.isConnected) {
-      publisher.publish(topic, msg)
-      log("[%s] published '%s'".format(topic, msg.toString))
-      true
-    } else {
-      log("[%s] can't publish anything, not conected".format(topic))
-      false
-    }
-  } catch {
-    case e: Exception => {
-      log("[%s] caught exception during publish: %s".format(topic, e.toString))
-      false
-    }
-  }
-
-  // Subscribe to any topic.  This could also be a list of topics.
-  def subscribe(topic: String): Boolean = try {
-    if (subscriber.isConnected) {
-      subscriber.subscribe(topic, qos)
-      log("Subscribed to [%s]".format(topic))
-      true
-    } else {
-      log("Can't subscribe to [%s], not connected".format(topic))
-      false
-    }
-  } catch {
-    case e: Exception => {
-      log("Exception during subscribe to [%s]: '%s'".format(topic, e.toString))
-      false
-    }
-  }
-
-  // Connect subscriber to the broker URI
-  def connectSubscriber(): Boolean = {
-    subscriber.connect(new MqttConnectOptions).waitForCompletion
-    if (subscriber.isConnected) {
-      log("subscriber is connected to %s".format(uri))
-      true
-    } else {
-      log("subscriber could not connect to %s".format(uri))
-      false
-    }
+  // Describe any throwables we catch.
+  def toString(t: Throwable): String = t match {
+    case e: ConnectException => "ConnectException: %s".format(e.toString)
+    case e: Exception => "Exception: %s".format(e.toString)
+    case _: Throwable => "Throwable: %s".format(t.toString)
   }
 
   // Connect publisher to the broker URI
-  def connectPublisher(): Boolean = {
-    publisher.connect(new MqttConnectOptions)
-    if (publisher.isConnected) {
-      log("publisher is connected to %s".format(uri))
-      true
+  def connectPublisher(name: String): Option[MqttClient] = try {
+    val pub = new MqttClient(uri, name, new MemoryPersistence())
+    pub.connect(new MqttConnectOptions)
+    if (pub.isConnected) {
+      report("publisher is connected to MQTT broker at %s".format(uri))
+      Some(pub)
     } else {
-      log("publisher could not connect to %s".format(uri))
-      false
+      report("publisher could not connect to MQTT broker at %s".format(uri))
+      None
+    }
+  } catch {
+    case t: Throwable => {
+      report("publisher could not connect to MQTT broker at %s".format(uri))
+      report(toString(t))
+      None
     }
   }
 
-  // Activate relayer
-  def start(): Boolean = if (
-    connectPublisher &&
-    connectSubscriber &&
-    subscribe(relayDst) &&
-    subscribe(relaySrc)
-  ) {
-    log("[%s] will be relayed to [%s]".format(relaySrc, relayDst))
-    true
-  } else false
+  // Connect subscriber to the broker URI on the relay destination topic
+  def connectSubscriber(name: String): Option[MqttAsyncClient] = try {
+    val sub = new MqttAsyncClient(uri, name, new MemoryPersistence())
+    sub.setCallback(this)
+    sub.connect(new MqttConnectOptions).waitForCompletion
+    sub.subscribe(relaySrc, qos)
+    sub.subscribe(relayDst, qos)
+    report("subscriber is connected to MQTT broker at %s".format(uri))
+    Some(sub)
+  } catch {
+    case t: Throwable => {
+      report("subscriber could not connect to MQTT broker at %s".format(uri))
+      report(toString(t))
+      None
+    }
+  }
 
-  // Deactivate relayer
-  // def stop(): Boolean = ...
+  // Publish a string
+  def publish(topic: String, text: String): Unit =
+    publish(topic, text.getBytes)
+
+  // Publish any byte array
+  def publish(topic: String, payload: Array[Byte]): Unit =
+    publish(topic, new MqttMessage(payload))
+
+  // Publish message
+  def publish(topic: String, msg: MqttMessage): Unit = try {
+    publisher.foreach(pub => {
+      pub.publish(topic, msg)
+      report("Published '%s' to %s".format(msg.toString, topic))
+    })
+  } catch {
+    case t: Throwable => { 
+      report("Could not publish '%s' to %s".format(msg.toString,topic))
+      report(toString(t))
+    }
+  }
 
   // Needed for MqttCallback extension
-  override def connectionLost(cause: Throwable): Unit =
-    log("Connection lost: %s".format(cause.toString))
+  override def connectionLost(t: Throwable): Unit = {
+    report("Connection to MQTT broker lost.")
+    report(toString(t))
+  }
 
   // Needed for MqttCallback extension
   override def deliveryComplete(token: IMqttDeliveryToken): Unit =
-    log("deliveryComplete: " + token.getMessage)
+    report("deliveryComplete: %s" + token.getMessage)
 
-  // Report any activity on our topic subscriptions.  Relay anything
-  // that arrives on the relay source topic.
+  // When a message is received on the relay source topic, publish it to the
+  // relay destination topic.
   override def messageArrived(topic: String, msg: MqttMessage): Unit = {
-    log("[%s] read '%s'".format(topic, msg.toString))
-    if (topic == relaySrc) relay(msg)
+    report("Read '%s' on %s".format(msg.toString, topic))
+    if (topic == relaySrc) {
+      report("Relaying to %s".format(relayDst))
+      publish(relayDst, msg)
+    }
   }
 }
 
