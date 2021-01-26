97765e015090b5f44f25a1556be35eea52c4aba6
Joseph Astier <jastier@gmail.com>
2021-01-25 17:04:31 -0700

    Added fields to the DialogAgentMessage class

--- src/main/scala/org/clulab/asist/messages/DialogAgentMessage.scala
+++ src/main/scala/org/clulab/asist/messages/DialogAgentMessage.scala
@@ -6,18 +6,24 @@
 //
 package org.clulab.asist
 
+object DialogAgentMessage{
+  val version: String = "0.1"
+}
+
 /** Part of the DialogAgentMessage class */
 case class DialogAgentMessageMsg(
   val source: String = "",
   val experiment_id: String = "",
   val timestamp: String = "",
-  val sub_type: String = "",
-  val version: String = ""
+  val sub_type: String = "Event:dialogue_event",
+  val version: String = DialogAgentMessage.version
 )
 
 /** Part of the DialogAgentMessage class */
 case class DialogAgentMessageData(
+  val participant_id: String = "",
   val text: String = "",
+  val source: DialogAgentMessageDataSource,
   val extractions:Seq[DialogAgentMessageDataExtraction] = Seq.empty
 )
 
@@ -29,10 +35,15 @@ case class DialogAgentMessageDataExtraction(
   val taxonomy_matches: Seq[(String, String)] = Seq.empty
 )
 
+/** Part of the DialogAgentMessageData class */
+case class DialogAgentMessageDataSource(
+  val source_type: String = "message_bus",
+  val topic: String = ""
+)
+
 /** Contains the full analysis data of one chat message */
 case class DialogAgentMessage (
   val header: MessageHeader,
   val msg: DialogAgentMessageMsg,
   val data: DialogAgentMessageData
 ) 
-
