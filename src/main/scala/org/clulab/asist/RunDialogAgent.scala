e56abb96ffc6b74df600488c4a7799b4fb08266f
Joseph Astier <jastier@gmail.com>
2021-02-05 12:13:21 -0700

    Moved default settings out of DialogAgentRunner and into their base classes

--- src/main/scala/org/clulab/asist/RunDialogAgent.scala
+++ src/main/scala/org/clulab/asist/RunDialogAgent.scala
@@ -3,49 +3,31 @@
  *
  *  updated:  2021 February
  *
- *  This class will test the DialogAgent in either MQTT or file mode
+ *  This object will run the DialogAgent in either MQTT or file mode
+ *  depending on user inputs
  */
 package org.clulab.asist
 
 object  RunDialogAgent extends App {
-  /** subscribe to these MQTT topics for messages */
-  val TOPIC_INPUT_OBS = "observations/chat"
-  val TOPIC_INPUT_ASR = "agent/asr"
-
-  /** publish message analysis to this MQTT topic */
-  val TOPIC_OUTPUT = "agent/tomcat_chatbot"
 
   /** display the usage hints */
   def usage: Unit = List(
-    "Usage",
-    "mqtt <host> <port number>",
+    "Usage:",
+    "  mqtt <host> <port number>",
     "or",
-    "file <input filename> <expected_results filename>",
+    "  file <input filename> <expected_results filename>",
   ).map(println)
 
-  /** Create an agent for the user args and keep it in scope */
-  val agent: Option[DialogAgent] = if(args.length == 3) args(0) match {
-    case "mqtt" => Some {
-      new DialogAgentMqtt(
-        host = args(1),
-        port = args(2),
-        topicInputObs = TOPIC_INPUT_OBS,
-        topicInputAsr = TOPIC_INPUT_ASR,
-        topicOutput = TOPIC_OUTPUT
-      )
-    }
-    case "file" => Some {
-      new DialogAgentFile(
-        inputFilename = args(1),
-        outputFilename = args(2)
-      )
-    }
+
+  /** Create an agent for the user args */
+  val agent: Option[DialogAgent] = args match {
+    case Array("mqtt", h: String, p: String) => 
+      Some(new DialogAgentMqtt(host = h, port = p))
+    case Array("file", i: String, o: String) => 
+      Some(new DialogAgentFile(inputFilename = i, outputFilename = o))
     case _ => {
       usage
       None
     }
-  } else {
-    usage
-    None
   }
 }
