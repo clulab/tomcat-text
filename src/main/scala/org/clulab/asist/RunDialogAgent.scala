3f2b0d8048ef355ee659061aea9b80fcb558049f
Joseph Astier <jastier@gmail.com>
2021-02-10 09:42:16 -0700

    Removed input validation from the DialogAgent runner, moved to DialogAgent.

--- src/main/scala/org/clulab/asist/RunDialogAgent.scala
+++ src/main/scala/org/clulab/asist/RunDialogAgent.scala
@@ -5,6 +5,7 @@
  *
  *  This object will run the DialogAgent in either message bus or 
  *  file mode depending on user inputs
+ *
  */
 package org.clulab.asist
 
@@ -12,42 +13,24 @@ import java.io.File
 
 object  RunDialogAgent extends App {
 
-  /** display the usage hints */
+  /** Show the usage hints */
   def usage: Unit = List(
     "",
     "To use %s on the message bus:".format(args(0)),
-    "%s mqtt [host] [port number]".format(args(0)),
+    "%s mqtt [host] [port]".format(args(0)),
     "",
-    "To use %s on a file:".format(args(0)),
-    "%s file [input_filename] [output_filename]".format(args(0)),
-    "",
-    "To use %s on a directory:".format(args(0)),
-    "%s dir [input_dirname] [output_filename]".format(args(0)),
+    "To use %s on a file or directory (first level only):".format(args(0)),
+    "%s file [inputFileOrDir] [outputFile]".format(args(0)),
     ""
   ).map(println)
-
-  /**  Find all the normal filenames in a directory */
-  def filenamesFromDir(dirname: String): List[String] = List(new File(dirname))
-    .filter(_.isDirectory)
-    .map(_.listFiles)
-    .toList
-    .flatten
-    .filter(_.isFile)
-    .map(_.getAbsolutePath)
 
 
   /** Create an agent based on the user args */
   val agent: Option[DialogAgent] = args match {
-    case Array("mqtt", h: String, p: String) => 
-      Some(new DialogAgentMqtt(host = h, port = p))
-    case Array("file", i: String, o: String) => Some(new DialogAgentFile(
-      inputFilenames = List(i), 
-      outputFilename = o)
-    )
-    case Array("dir", d: String, o: String) => Some(new DialogAgentFile(
-      inputFilenames = filenamesFromDir(d),
-      outputFilename = o)
-    )
+    case Array("mqtt", host: String, port: String) => 
+      Some(new DialogAgentMqtt(host, port))
+    case Array("file", inputFile: String, outputFile: String) => 
+      Some(new DialogAgentFile(inputFile, outputFile))
     case _ => {
       usage
       None
