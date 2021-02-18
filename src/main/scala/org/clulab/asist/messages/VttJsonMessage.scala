6c8457ed92d0f41eff974bd52ee657b1093c1d76
Joseph Astier <jastier@gmail.com>
2021-02-18 09:51:44 -0700

    Moved 'participant_id' from 'msg' to 'data'

--- src/main/scala/org/clulab/asist/messages/VttJsonMessage.scala
+++ src/main/scala/org/clulab/asist/messages/VttJsonMessage.scala
@@ -19,7 +19,6 @@ case class VttJsonMessageHeader (
 case class VttJsonMessageMsg(
   val timestamp: String = null,  // "2021-02-04T19:41:57.205166Z"
   val experiment_id: String = null,  // 
-  val participant_id: String = null,  // "firstname lastname"
   val trial_id: String = null,  // 
   val message_type: String = null, // "observation"
   val version: String = null,  // "0.1"
@@ -30,6 +29,7 @@ case class VttJsonMessageMsg(
 case class VttJsonMessageData(
   val asr_system: String = null,  // "Zoom"
   val source_filename: String = null, // "AudioTranscriptFile.vtt"
+  val participant_id: String = null,  // "firstname lastname"
   val caption_start: String = null, // "00:00:24.090"
   val caption_end: String = null,  // "00:00:32.040"
   val text: String = null // "We are about to start the experiment."
