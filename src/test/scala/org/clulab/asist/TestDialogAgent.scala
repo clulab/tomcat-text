fd8a3590c0896741c7795f1e869900f3cf04ab15
Joseph Astier <jastier@gmail.com>
2021-02-19 15:30:36 -0700

    Added a working sbt test case

--- src/test/scala/org/clulab/asist/TestDialogAgent.scala
+++ src/test/scala/org/clulab/asist/TestDialogAgent.scala
@@ -3,20 +3,19 @@
  *
  *  Updated:  2021 February
  *
- *  Test the DialogAgent by comparing its responses on the message bus with 
- *  known standards
+ *  Test the DialogAgent by comparing its output with known standards
  */
 package org.clulab.asist
 
-class TestDialogAgent exends DialogAgent {
+import org.scalatest.{FlatSpec, Matchers}
 
-  "DialogAgent" should "Create DialogAgentMessage properly" in {
+class TestDialogAgent extends FlatSpec with DialogAgent with Matchers  {
+
     val source_type = "CI testing"
     val source_name = "sbt test"
     val experiment_id = "test 1"
     val participant_id = "jastier"
     val text = "Saving yellow victim"
-
     val result = toDialogAgentMessage(
       source_type,
       source_name,
@@ -25,7 +24,11 @@ class TestDialogAgent exends DialogAgent {
       text
     )
 
+  "DialogAgent" should "create output correctly" in {
+    result.data.source.source_type should be (source_type)
+    result.data.source.source_name should be (source_name)
     result.msg.experiment_id should be (experiment_id)
+    result.data.participant_id should be (participant_id)
+    result.data.text should be (text)
   }
-
 }
