7d158c2a44351aff4b9016e2764307693cea7c02
Joseph Astier <jastier@gmail.com>
2021-02-25 15:53:53 -0700

    The DialogAgentWebVtt class now builds

--- src/main/scala/org/clulab/asist/DialogAgentWebVtt.scala
+++ src/main/scala/org/clulab/asist/DialogAgentWebVtt.scala
@@ -14,14 +14,14 @@ package org.clulab.asist
 import java.io.{FileInputStream, File, PrintWriter}
 import org.slf4j.LoggerFactory
 import scala.io.{BufferedSource, Source}
-import scala.util.Try
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 import com.crowdscriber.caption.common.Vocabulary.SubtitleBlock
 import com.crowdscriber.caption.vttdissector.VttDissector
 import org.htmlcleaner.HtmlCleaner
+import java.time.Clock
 
 
-class DialogAgentVtt(
+class DialogAgentWebVtt(
     val inputFilename: String = "",
     val outputFilename: String = "output_events.json"
 ) extends DialogAgent 
@@ -40,23 +40,21 @@ class DialogAgentVtt(
   subs match {
     case Success(s) => {
       s.foreach(sub => println(toJson(toVttJsonMessage(sub))))
-
       stream.close
     }
     case Failure(f) => println("failure")
   }
 
-  //TODO See if the 'caption.start' in vtt_to_json_msgs is a Time object.
   //TODO How do we handle multiple lines?  Concatenate?  
   //TODO Break the first line on ':', if it exists then participant_id is before it
-  //TODO Run this on the entire .vtt file and just print the subs to see them.
+  //TODO Run this on an entire .vtt file and just print the SubtitleBlocks to see them
 
   def toVttJsonMessage(sub: SubtitleBlock): VttJsonMessage = {
     val timestamp = Clock.systemUTC.instant.toString
     val version = "0.1"
     val lines: Seq[String] = sub.lines
-    val start: Time = sub.start
-    val end: Time = sub.end
+    val start: Int = sub.start
+    val end: Int = sub.end
     VttJsonMessage(
       VttJsonMessageHeader(
         timestamp = timestamp,
@@ -72,11 +70,11 @@ class DialogAgentVtt(
       ),
       VttJsonMessageData(
         asr_system = "Zoom",
-        source_filename = file.path,
-        participant_id: String = null,  // "firstname lastname"
-        caption_start = sub.start  // Time
-        caption_end = sub.end  // Time
-        text: String = null // "We are about to start the experiment."
+        source_filename = file.getPath,
+        participant_id = null,  // "firstname lastname"
+        caption_start = start.toString,  // milliseconds
+        caption_end = end.toString,  // milliseconds
+        text = null // "We are about to start the experiment."
       )
     )
   }
