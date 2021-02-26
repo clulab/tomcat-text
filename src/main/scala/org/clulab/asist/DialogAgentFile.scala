250d7d077160114bd937bdf428d6eea72e52b4db
Joseph Astier <jastier@gmail.com>
2021-02-26 15:51:15 -0700

    File agent will now translate .vtt directly to chat_anaysis_format

--- src/main/scala/org/clulab/asist/DialogAgentFile.scala
+++ src/main/scala/org/clulab/asist/DialogAgentFile.scala
@@ -7,15 +7,26 @@
  *  on directories and individual files.   Directories are 
  *  processed one level deep.
  *
- *  inputFilename  - Either a file or directory.
+ *  This class currently only reads .vtt, but could be extended to others
  *
- *  outputFilename - The results of processing the input filenames.
+ *  inputFilename  - Either a web_vtt file or directory of web_vtt files
+ *
+ *  outputFilename - The concatenated results of processing the input files.
  */
 package org.clulab.asist
 
 import java.io.{File, PrintWriter}
 import org.slf4j.LoggerFactory
 import scala.io.{BufferedSource, Source}
+import java.io.{FileInputStream, File, PrintWriter}
+import org.slf4j.LoggerFactory
+import scala.io.{BufferedSource, Source}
+import scala.util.{Failure, Success, Try}
+import com.crowdscriber.caption.common.Vocabulary.SubtitleBlock
+import com.crowdscriber.caption.vttdissector.VttDissector
+import org.htmlcleaner.HtmlCleaner
+import java.time.Clock
+
 
 class DialogAgentFile(
     val inputFilename: String = "",
@@ -35,21 +46,64 @@ class DialogAgentFile(
   /** Process one file */
   def processFile(filename: String, output: PrintWriter): Unit = try {
     logger.info("Reading %s".format(filename))
-    val source = Source.fromFile(new File(filename))
-    val lines = source.getLines
-    while (lines.hasNext)
-      toVttJsonMessage(lines.next).map(a =>
-        output.write("%s\n".format(toJson(toDialogAgentMessage(a)))))
-    source.close
+    val stream = new FileInputStream(new File(filename))
+    parseInputStream(stream, filename, output)
+    stream.close
     logger.info("Closed %s".format(filename))
   } catch {
     case t: Throwable => {
-      logger.error("Problem reading from %s".format(filename))
+      logger.error("Could not open '%s' for reading.".format(filename))
       logger.error(t.toString)
-      None
     }
   }
-  
+
+  /** Run a VttDissector on the file contents */
+  def parseInputStream(stream: FileInputStream, filename: String, output: PrintWriter): Unit = 
+    VttDissector(stream) match {
+      case Success(blocks) => blocks.map(block => {
+        val json = toJson(toDialogAgentMessage(block, filename))
+        output.write("%s\n".format(json))
+      })
+      case Failure(f) => {
+        logger.error("VttDissector could not parse input")
+        logger.error(f.toString)
+      }
+    }
+
+
+  def toDialogAgentMessage (sub: SubtitleBlock, filename: String): DialogAgentMessage = {
+    val timestamp = Clock.systemUTC.instant.toString
+    val version = "0.1"
+    val lines: Seq[String] = sub.lines
+    val start: Int = sub.start
+    val end: Int = sub.end
+    val extractions: Seq[DialogAgentMessageDataExtraction] = Seq.empty
+
+    DialogAgentMessage(
+      MessageHeader(
+        timestamp = timestamp,
+        message_type = "observation",
+        version = version
+      ),
+      DialogAgentMessageMsg(
+        source = "tomcat_text_analyzer",
+        experiment_id = null,
+        timestamp = timestamp,
+        sub_type = "Event:dialogue_event",
+        version = version
+      ),
+      DialogAgentMessageData(
+        participant_id = null,
+        text = null, // needed
+        DialogAgentMessageDataSource(
+          source_type = null, // needed
+          source_name = filename
+        ),
+        extractions // needed
+      )
+    )
+  }
+
 
   // open the output stream and process the files
   try {
