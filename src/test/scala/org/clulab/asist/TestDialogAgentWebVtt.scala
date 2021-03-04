dec68b6acb424c685365445807abbaa874595ffc
Joseph Astier <jastier@gmail.com>
2021-03-04 14:56:53 -0700

    Added header and msg readback tests

--- src/test/scala/org/clulab/asist/TestDialogAgentWebVtt.scala
+++ src/test/scala/org/clulab/asist/TestDialogAgentWebVtt.scala
@@ -1,15 +1,17 @@
 /**
  *  Authors:  Joseph Astier, Adarsh Pyarelal
  *
- *  Updated:  2021 February
+ *  Updated:  2021 March
  *
  *  Test the WebVtt agent by reading a test file and comparing its the output
  *  with known standards
  */
 package org.clulab.asist
 
-import java.io.{BufferedReader, File, PrintWriter}
+import java.io.{File, PrintWriter}
+import scala.io.Source
 import org.scalatest.{FlatSpec, Matchers}
+import org.slf4j.LoggerFactory
 
 
 class TestDialogAgentWebVtt 
@@ -21,58 +23,86 @@ class TestDialogAgentWebVtt
   val inputFilename = "TestDialogAgentWebVtt_input.vtt"
   val outputFilename = "TestDialogAgentWebVtt_output.json"
 
-  // create the test file
-  def createTestFile: Boolean = {
-    val lines = List(
-      "WEBVTT",
-      "",
-      "1",
-      "00:00:03.600 --> 00:00:15.450",
-      "Jane Doe: Great. So thank you, you will be participating in the simulated search and rescue mission in the Minecraft environment, the entire experiment will take about two and half hours.",
-      "",
-      "2",
-      "00:00:16.320 --> 00:00:23.730",
-      "But before we begin, I just want to emphasize that, as this is an experiment, we may say things formerly"
+  private val logger = LoggerFactory.getLogger(this.getClass())
+
+
+  // create the test input data
+  val inputLines = List(
+    "WEBVTT",
+    "",
+    "1",
+    "00:00:03.600 --> 00:00:15.450",
+    "Jane Doe: Great. So thank you, you will be participating in the simulated search and rescue mission in the Minecraft environment, the entire experiment will take about two and half hours.",
+    "",
+    "2",
+    "00:00:16.320 --> 00:00:23.730",
+    "Jane Doe But before we begin, I just want to emphasize that, as this is an experiment, we may say things formerly",
+    ""
     )
 
-    try {
+
+  // should really abort the test if these exist, and tell the user to move them aside 
+  "web_vtt test initialization" should "remove existing test files" in {
+    if(new File(inputFilename).exists) new File(inputFilename).delete
+    if(new File(outputFilename).exists) new File(outputFilename).delete
+    val inputExists = new File(inputFilename).exists
+    val outputExists = new File(outputFilename).exists
+    inputExists should be (false)
+    outputExists should be (false)
+  }
+
+
+  // write the test input file
+  it should "write the test input file '%s'".format(inputFilename) in {
+    val inputFileWritten = try {
       val writer = new PrintWriter(new File(inputFilename))
-      lines.map(line => writer.write("%s\n".format(line)))
+      inputLines.map(line => writer.write("%s\n".format(line)))
       writer.close
+      logger.info("test input file written successfully")
       true
     } catch {
       case t: Throwable => {
-        println("Could not create test file %s\n".format(inputFilename))
+        logger.error("Could not write test file %s\n".format(inputFilename))
+        logger.error(t.toString)
         false
       }
     }
+    inputFileWritten should be (true)
   }
 
-  val setupOK = createTestFile
-  val dialogAgentWebVtt = new DialogAgentWebVtt(inputFilename, outputFilename)
-
-  "Initialization" should "write the test file correctly" in {
-    setupOK should be (true)
-  }
-  "Test run" should "create MQTT agent" in {
-    dialogAgentWebVtt should not be (None)
-  }
-
-  // at this point the results are now in the output file.   Read it back 
-  // and inspect the contents.
-  def readbackOutput: Boolean = {
-    val reader = new BufferedReader(new File(outputFilename))
-    test(reader.lines, List.empty)
-  }
-
-  def lines(stream: Stream[String], ret: List[String]): List[String] = {
-    if(stream.hasNext) lines(stream, stream.next::ret)
-    else ret
+  // if the input file was written successfully, then we test the web_vtt agent:
+  it should "write the output file '%s'".format(outputFilename) in {
+    val runOK = DialogAgentWebVtt(inputFilename, outputFilename)
+    val outputExists = new File(outputFilename).exists
+    runOK should be (true)
+    outputExists should be (true)
   }
 
 
-  def test(article: List[String]): Boolean = {
-    true
+  val outputSource = Source.fromFile(outputFilename)
+  val output= outputSource
+    .getLines
+    .toList
+    .map(toDialogAgentMessage(_))
+    .flatten
+  outputSource.close
+
+
+  "Test output" should "have two elements" in {
+    output.size should be (2)
+  }
+
+  "First output element" should "have correct header info" in {
+    val header = output(0).header
+    header.message_type should be ("event")
+    header.version should be ("0.1")
+  }
+  it should "have correct msg info" in {
+    val msg = output(0).msg
+    msg.source should be ("tomcat_textAnalyzer")
+    msg.experiment_id should be (null)
+    msg.sub_type should be ("Event:dialogue_event")
+    msg.version should be ("0.1")
   }
 
 }
