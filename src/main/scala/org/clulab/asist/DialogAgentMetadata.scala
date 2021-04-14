d694906f6f960efe23ac334a5bac50ead38805df
Joseph Astier <jastier@gmail.com>
2021-04-13 17:48:44 -0700

    Moved topics to AgentJson base class

--- src/main/scala/org/clulab/asist/DialogAgentMetadata.scala
+++ src/main/scala/org/clulab/asist/DialogAgentMetadata.scala
@@ -17,78 +17,31 @@
 package org.clulab.asist
 
 import scala.io.Source
-import java.io.{File, PrintWriter}
-import org.slf4j.LoggerFactory
-import spray.json.DefaultJsonProtocol._
-import spray.json.JsonParser
+import java.io.PrintWriter
 
 class DialogAgentMetadata(
     val inputFilename: String = "",
     val outputFilename: String = "", 
     override val nMatches: Int = 0
-) extends DialogAgent 
-    with DialogAgentJson 
+) extends AgentJson 
     with AgentFile {
-
-  val topicChat: String = "minecraft/chat"
-  val topicUazAsr: String = "agent/asr/final"
-  val topicAptimaAsr: String = "status/asistdataingester/userspeech"
 
   val source_type = "metadata_file"
 
-  private lazy val logger = LoggerFactory.getLogger(this.getClass())
-
-  logger.info("Using input file '%s'".format(inputFilename))
-  logger.info("Using output file '%s'".format(outputFilename))
+  processFiles(inputFilename, outputFilename)
 
   /** Wrangle one metadata file 
    * @param filename a single input file
    * @param output Printwriter to the output file
    * @return true if the operation succeeded
    */
-  def processFile(filename: String, output: PrintWriter): Boolean = try {
-    logger.info("Reading input file %s".format(filename))
+  override def processFile(filename: String, output: PrintWriter): Unit = {
     val bufferedSource = Source.fromFile(filename)
-    bufferedSource.getLines.foreach(line => processLine(line, output))
-    bufferedSource.close
-    logger.info("Closed input file %s".format(filename))
-    true
-  } catch {
-    case t: Throwable => {
-      logger.error("Problem reading input file '%s'".format(filename))
-      logger.error(t.toString)
-      false
+    for (line <- bufferedSource.getLines) {
+      outputJson(source_type, filename, line).map(a =>
+        output.write("%s\n".format(a))
+      )
     }
-  }
-
-  def processLine(json: String, output: PrintWriter): Unit = 
-    toMetadataMessage(json).foreach(metadata => 
-      toMessage(metadata).foreach(message =>
-        output.write("%s\n".format(toJson(message)))
-      )   
-    )
-
-
-  /** The participant_id field is different in each type */
-  def participant_id(md: MetadataMessage): Option[String] = md.topic match {
-    case `topicChat` => Some(md.data.sender)
-    case `topicUazAsr` => Some(md.data.participant_id)
-    case `topicAptimaAsr` => Some(md.data.playername)
-    case _ => None
-  }
-
-
-  /** publish the Dialog Agent analysis of message data
-   *  @param topic: The message bus topic where the message was published
-   *  @param json:  A json representation of a message struct
-   */
-  def toMessage(md: MetadataMessage): Option[DialogAgentMessage] = 
-    participant_id(md).map(id => toDialogAgentMessage( 
-      source_type,
-      md.topic,
-      md.msg,
-      id,
-      md.data.text
-    )
-  )
+    bufferedSource.close
+  } 
 }
