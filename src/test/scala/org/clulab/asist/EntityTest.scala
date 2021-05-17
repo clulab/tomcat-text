f40a93656cd3eda44d63eb75e0838692991dd7d8
remo-help <1163925remo@gmail.com>
2021-05-17 12:15:54 +0200

    tests: -moved a test to CommitmentTest.scala -established EntityTest.scala
    maintenance:
    -established rules for RED/GREEN/BLUE and moved them to "PLAYER" in the taxonomy to reflect upcoming changes

--- src/test/scala/org/clulab/asist/EntityTest.scala
+++ src/test/scala/org/clulab/asist/EntityTest.scala
@@ -16,6 +16,21 @@ class EntityTest extends BaseTest {
     testMention(mentions, victim_mention)
   }
 
+  passingTest should "Recognize rubble, red, blue, green" in {
+    val doc = extractor.annotate("Green, do you see any rubble here? This is red, one victim in sight. Can you confirm blue?")
+    val mentions = extractor.extractFrom(doc)
+
+    val red_mention = DesiredMention("Red", "red")
+    val green_mention = DesiredMention("Green", "Green")
+    val blue_mention = DesiredMention("Blue", "blue")
+    val rubble_mention = DesiredMention("Rubble", "rubble")
+
+    testMention(mentions, red_mention)
+    testMention(mentions, green_mention)
+    testMention(mentions, blue_mention)
+    testMention(mentions, rubble_mention)
+  }
+
 
 
 
