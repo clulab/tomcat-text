244cb6438b030d26d731e58923fdb4f8538718cb
remo-help <1163925remo@gmail.com>
2021-05-24 15:23:20 +0200

    tests: - added tests for the new rules - fixed some tests that were broken by the new rules
    rules:
    - fixed the blocking rules

---
+++ src/test/scala/org/clulab/asist/text/TestCommunications.scala
@@ -0,0 +1,34 @@
+package org.clulab.asist.text
+
+import org.clulab.asist.BaseTest
+// For testing team communications
+
+class TestCommunications extends BaseTest {
+
+  behavior of "team_communication.yml"
+
+
+  it should "Parse existential constructions" in {
+    val text = "There is a medkit in the library. "
+
+    val mentions = extractor.extractFromText(text)
+    val medkit = DesiredMention("MedKit", "medkit")
+    val location = DesiredMention("Infrastructure", "library")
+    val ex1 = DesiredMention("KnowledgeSharing", "There is a medkit in the library", Map("location" -> Seq(location), "exists" -> Seq(medkit)))
+
+    testMention(mentions, ex1)
+
+  }
+
+it should "Parse existential constructions" in {
+
+  val text =  "There is a victim in here."
+
+   val mentions = extractor.extractFromText(text)
+   val here = DesiredMention("Deictic", "here")
+   val victim = DesiredMention("Victim", "victim")
+   val ex2 = DesiredMention("KnowledgeSharing", "There is a victim in here", Map("exists" -> Seq(victim), "location" -> Seq(here)))
+
+  testMention(mentions, ex2)
+}
+}
