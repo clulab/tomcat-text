16addf131ffd38d46147df0fb28251b49afeeca6
remo-help <1163925remo@gmail.com>
2021-05-24 16:00:26 +0200

    rules: - added a token rule for "fire" which was miraculously not present

--- src/test/scala/org/clulab/asist/text/TestCommunications.scala
+++ src/test/scala/org/clulab/asist/text/TestCommunications.scala
@@ -5,10 +5,10 @@ import org.clulab.asist.BaseTest
 
 class TestCommunications extends BaseTest {
 
-  behavior of "team_communication.yml"
+ // behavior of "team_communication.yml"
 
 
-  it should "Parse existential constructions" in {
+  passingTest should "Parse existential constructions" in {
     val text = "There is a medkit in the library. "
 
     val mentions = extractor.extractFromText(text)
@@ -20,14 +20,13 @@ class TestCommunications extends BaseTest {
 
   }
 
-it should "Parse existential constructions" in {
+  passingTest should "Parse existential constructions 2" in {
+    val text =  "There is a victim in here."
 
-  val text =  "There is a victim in here."
-
-   val mentions = extractor.extractFromText(text)
-   val here = DesiredMention("Deictic", "here")
-   val victim = DesiredMention("Victim", "victim")
-   val ex2 = DesiredMention("KnowledgeSharing", "There is a victim in here", Map("exists" -> Seq(victim), "location" -> Seq(here)))
+    val mentions = extractor.extractFromText(text)
+    val here = DesiredMention("Deictic", "here")
+    val victim = DesiredMention("Victim", "victim")
+    val ex2 = DesiredMention("KnowledgeSharing", "There is a victim in here", Map("exists" -> Seq(victim), "location" -> Seq(here)))
 
   testMention(mentions, ex2)
 }
