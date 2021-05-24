4ce60db9bdec3be1cd07335b8978ef07f8e362aa
Joseph Astier <jastier@gmail.com>
2021-05-24 15:42:30 -0700

    Test rules parsing correctly

--- src/main/scala/org/clulab/asist/YmlReader.scala
+++ src/main/scala/org/clulab/asist/YmlReader.scala
@@ -8,8 +8,9 @@
  * Snake YAML implementation based on: 
  *    https://www.baeldung.com/java-snake-yaml
  *
- * Additional documentation from:
+ * Additional reading:
  *    https://bitbucket.org/asomov/snakeyaml/wiki/Documentation
+ *    https://www.javadoc.io/doc/org.yaml/snakeyaml/1.19/org/yaml/snakeyaml/Yaml.html
  *
  * @param inputFilename A file or directory of files to process.
  * @param outputFilename The results of all file processing are written here
@@ -62,18 +63,11 @@ class YmlReader(
     filename.substring(filename.lastIndexOf(".")) match {
       case ".yml" | ".yaml" => {
         logger.info("processing '%s' ...".format(filename))
-
-        try {
-          val source = scala.io.Source.fromFile(filename)
-          val lines = source.getLines.mkString("\n")
-          processYaml(lines)
-          source.close
-        }
-        catch {
-          case t: Throwable => {
-            logger.error("File not found '%s'".format(filename))
-          }
-        }
+        val source = scala.io.Source.fromFile(filename)
+        val lines = source.getLines
+        val str = lines.mkString("\n")
+        processAllYaml(str)
+        source.close
       }
       case _ => {
         logger.error("Can't process this type of file '%s'".format(filename))
@@ -82,10 +76,44 @@ class YmlReader(
     }
 
 
-  def processYaml(str: String): Unit = {
+  // String input from one YAML file.
+  def processAllYaml(str: String): Unit = {
+    System.out.println("\nPROCESSING ALL YAML: %s".format(str))
     val yaml = new Yaml
-    val javaIterable = yaml.load(str)
-    val javaMap = javaIterable.asInstanceOf[java.util.Map[String, ArrayList[Any]]]
+    val javaIterable = yaml.loadAll(str)
+    val javaIterator = javaIterable.iterator
+
+    /* the iterator has the entire string as a single object:
+    
+       {
+         vars=org/clulab/asist/grammars/vars.yml, 
+         hammer_triggers=hammer|mallet|sledgehammer, 
+         rulepriority=2, 
+         rules=[
+           {
+             name=sonic_screwdriver, label=Sonic Screwdriver, keep=true
+           }, 
+           {
+             name=tricorder, label=Tricorder, keep=true
+           }
+         ]
+       }
+
+     */
+      
+    while(javaIterator.hasNext) processYamlObject(javaIterator.next)
+  }
+
+
+  // a comlete YAML document that was read first pass as a java Object
+  def processYamlObject(javaObj: Any): Unit = {
+    System.out.println("YAML OBJECT: %s".format(javaObj))
+    val javaMap = javaObj.asInstanceOf[java.util.Map[String,Any]]
+    processYamlMap(javaMap)
+  }
+
+  def processYamlMap(javaMap: java.util.Map[String,Any]): Unit = {
+    System.out.println("YAML MAP: %s".format(javaMap))
     val map = javaMap.asScala
     val keys = map.keySet
     System.out.println("number of keys found: %d".format(keys.size))
@@ -96,42 +124,34 @@ class YmlReader(
       System.out.println("KEY: %s".format(keyIter.next))
     }
 
-    showRules(map.getOrElse("rules", ""))
+    // show the rules if there are rules to be shown
+    map.get("rules").foreach(a => processYamlRules(a))
   }
 
 
-  def showRules(rules: java.io.Serializable): Unit = {
-    System.out.println ("RULES:")
-    System.out.println (rules)
+  // YAML rules are an ArrayList of maps
+  // java object will come in as Some(arraylist of maps)
 
-    /*
-    System.out.println("number of rules found: %d".format(rules.size))
-    val ruleIter = rules.iterator
-    while(ruleIter.hasNext){
-      val element = ruleIter.next
-      val rule = element.asInstanceOf[java.util.Map[String, ArrayList[Any]]]
-      showRule(rule)
-    }
-    */
+  def processYamlRules(obj: Any): Unit = {
+    System.out.println("RULES: %s".format(obj.toString))
+    val rules = obj.asInstanceOf[java.util.ArrayList[java.util.Map[String,Any]]]
+    val iterator = rules.iterator
+    while(iterator.hasNext) processYamlRule(iterator.next)
   }
 
 
-  def showRule(map: java.util.Map[String, ArrayList[Any]]) {
-    System.out.println ("RULE:")
-    System.out.println (map.toString)
-    System.out.println
-    /*
-    val map: LinkedHashMap[String, ArrayList[Any]] = 
-      yaml.load(rule).asInstanceOf[LinkedHashMap[String, ArrayList[Any]]]
+  // Each YAML rule is a map.   Presumably of arbitrary depth.
+  def processYamlRule(rule: java.util.Map[String,Any]): Unit = {
+    System.out.println("RULE: %s".format(rule.toString))
+    val map = rule.asScala
     val keys = map.keySet
     System.out.println("number of keys found: %d".format(keys.size))
 
     val keyIter = keys.iterator
 
     while(keyIter.hasNext) {
-      System.out.println("RULE KEY: %s".format(keyIter.next))
+      System.out.println("KEY: %s".format(keyIter.next))
     }
-    */
   }
 
   this(inputFilename, outputFilename)
