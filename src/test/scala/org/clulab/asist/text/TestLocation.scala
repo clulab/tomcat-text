45a0f03533faefc338deda061877ca8c567b0525
Megh <meghavarshini@gmail.com>
2021-06-02 10:20:57 -0700

    skeleton file

--- src/test/scala/org/clulab/asist/text/TestLocation.scala
+++ src/test/scala/org/clulab/asist/text/TestLocation.scala
@@ -6,20 +6,20 @@ class TestLocation extends BaseTest {
 
   behavior of "AsistEngine"
 
-//  failingTest should "Parse defeat events properly" in {
-//    val doc = extractor.annotate("To progress I'm going to kill the zombies")
-//    val mentions = extractor.extractFrom(doc)
-//
-//    val self_mention = DesiredMention("Self", "I")
-//    val foe_mention = DesiredMention("Foe", "zombies")
-//    val defeat_mention = DesiredMention("Defeat", "I'm going to kill the zombies",
-//      Map("agent" -> Seq(self_mention),
-//        "target" -> Seq(foe_mention)))
-//
-//    testMention(mentions, self_mention)
-//    testMention(mentions, foe_mention)
-//    testMention(mentions, defeat_mention)
-//  }
+  failingTest should "Parse defeat events properly" in {
+    val doc = extractor.annotate("To progress I'm going to kill the zombies")
+    val mentions = extractor.extractFrom(doc)
+
+    val self_mention = DesiredMention("Self", "I")
+    val foe_mention = DesiredMention("Foe", "zombies")
+    val defeat_mention = DesiredMention("Defeat", "I'm going to kill the zombies",
+      Map("agent" -> Seq(self_mention),
+        "target" -> Seq(foe_mention)))
+
+    testMention(mentions, self_mention)
+    testMention(mentions, foe_mention)
+    testMention(mentions, defeat_mention)
+  }
   /*
    // Target: location
    passingTest should "Parse location tokens properly" in {
