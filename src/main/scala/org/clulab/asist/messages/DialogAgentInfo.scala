0245a6ae15dcce8ee4d23dcf76946f027ed3735b
Joseph Astier <jastier@gmail.com>
2021-06-07 10:05:08 -0700

    Added DialogAgentInfo data structure

---
+++ src/main/scala/org/clulab/asist/messages/DialogAgentInfo.scala
@@ -0,0 +1,28 @@
+/**
+ *  Authors:  Joseph Astier, Adarsh Pyarelal
+ *
+ *  updated:  2021 June
+ *
+ *  Dialog Agent Info, based on:
+ *
+ *  https://gitlab.asist.aptima.com/asist/testbed/-/blob/develop/MessageSpecs/Agent/versioninfo/agent_versioninfo.md
+ *
+ *  This message is generated whenever there is a trial start message 
+ *  published on the message bus
+ */
+package org.clulab.asist
+
+
+/** Part of the DialogAgentInfo class */
+case class DialogAgentInfoData(
+  val agent_name: String = "Not Set"
+  val version: String = "Not Set"
+  val owner: String = "Not Set"
+)
+
+/** Contains the full analysis data of one chat message */
+case class DialogAgentInfo (
+  val header: CommonHeader,
+  val msg: CommonMsg,
+  val data: DialogAgentInfoData
+) 
