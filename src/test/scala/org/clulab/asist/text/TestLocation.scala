87049cd3e1b7771f4545dec23e155cc08699e47d
Megh <meghavarshini@gmail.com>
2021-06-14 10:12:19 -0700

    added a some test valMentions

--- src/test/scala/org/clulab/asist/text/TestLocation.scala
+++ src/test/scala/org/clulab/asist/text/TestLocation.scala
@@ -4,36 +4,49 @@ import org.clulab.asist.BaseTest
 
 class TestLocation extends BaseTest {
 
-  behavior of "AsistEngine"
+  behavior of "locations.yml"
 
-  failingTest should "Parse defeat events properly" in {
-    val doc = extractor.annotate("To progress I'm going to kill the zombies")
-    val mentions = extractor.extractFrom(doc)
+  passingTest should "Find location words in the text" in {
+    val text = "First, head North, then progress to the first zone on the South-East. " +
+    "What does the map say about the South-West? " +
+    "Go North-East, clear the area. " +
+    "Can you head to the first floor. " +
+    "Three victims in the back room. " +
+    "Keep going till you are out of the first part. " +
+    ""
 
-    val self_mention = DesiredMention("Self", "I")
-    val foe_mention = DesiredMention("Foe", "zombies")
-    val defeat_mention = DesiredMention("Defeat", "I'm going to kill the zombies",
-      Map("agent" -> Seq(self_mention),
-        "target" -> Seq(foe_mention)))
 
-    testMention(mentions, self_mention)
-    testMention(mentions, foe_mention)
-    testMention(mentions, defeat_mention)
+    val mentions = extractor.extractFromText(text)
+
+    val firstfloor = DesiredMention("Infrastructure", "first floor")
+    val backroom = DesiredMention("Infrastructure", "back room")
+    val backroom = DesiredMention("Infrastructure", "back room")
+
+    val north_mention = DesiredMention("North", "North")
+    val east_mention = DesiredMention("East", "East")
+    val south_mention = DesiredMention("South", "South")
+    val west_mention = DesiredMention("West", "West")
+
+    val ne1_mention = DesiredMention("NorthEast", "North-East")
+    val ne2_mention = DesiredMention("NorthEast", "North East")
+    val ne3_mention = DesiredMention("NorthEast", "north-east")
+
+    val nw1_mention = DesiredMention("NorthWest", "North-West")
+    val nw2_mention = DesiredMention("NorthWest", "North West")
+    val nw3_mention = DesiredMention("NorthWest", "north-west")
+
+    val se1_mention = DesiredMention("SouthEast", "South-East")
+    val se2_mention = DesiredMention("SouthEast", "South East")
+    val se3_mention = DesiredMention("SouthEast", "south-east")
+
+    val sw1_mention = DesiredMention("SouthWest", "South-West")
+    val sw2_mention = DesiredMention("SouthWest", "South West")
+    val sw3_mention = DesiredMention("SouthWest", "south-west")
+
+    val zone_mention = DesiredMention("Zone", "zone")
+
+    testMention(mentions, north_mention)
+    testMention(mentions, east_mention)
+    testMention(mentions, zone_mention)
   }
-  /*
-   // Target: location
-   passingTest should "Parse location tokens properly" in {
-
-    val doc = extractor.annotate("Go north, then turn east, continue west before moving south")
-    val mentions = extractor.extractFrom(doc)
-
-    val planning_mention = DesiredMention("Planning", "Before")
-    val team_mention = DesiredMention("Team", "we")
-    val precedence_mention = DesiredMention("Precedence", "Before")
-
-    testMention(mentions, planning_mention)
-    testMention(mentions, team_mention)
-    testMention(mentions, precedence_mention)
-  }
-  */
 }
