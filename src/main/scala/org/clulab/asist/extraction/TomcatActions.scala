5cc0c556229b3cb0f0e33c29da670d327f996381
Joseph Astier <jastier@gmail.com>
2021-07-07 11:31:31 -0700

    Merge conflict resolution, going with master version

--- src/main/scala/org/clulab/asist/extraction/TomcatActions.scala
+++ src/main/scala/org/clulab/asist/extraction/TomcatActions.scala
@@ -1,10 +1,16 @@
-package org.clulab.asist.extraction
+package org.clulab.asist
+
 
 import com.typesafe.scalalogging.LazyLogging
-import org.clulab.asist.extraction.TomcatRuleEngine._
 import org.clulab.odin._
+import org.clulab.asist.AsistEngine._
 
-class TomcatActions() extends Actions with LazyLogging {
+import scala.collection.mutable.ArrayBuffer
+
+class TomcatActions(
+    val timeintervals: (ArrayBuffer[Int], ArrayBuffer[Int], ArrayBuffer[Int])
+) extends Actions
+    with LazyLogging {
 
   def passThrough(
       mentions: Seq[Mention],
@@ -53,6 +59,63 @@ class TomcatActions() extends Actions with LazyLogging {
     localState.lookUpTable.values.toStream.flatten.distinct.toVector
   }
 
+  def requireSubjectVerbInversion(mentions: Seq[Mention], state: State = new State()): Seq[Mention] = {
+    // trigger should be before all the arguments
+    // todo: revisit when the no_agent branch merged
+    // FIXME!!
+    for {
+      mention <- mentions
+      triggerStart = mention.asInstanceOf[EventMention].trigger.start
+      leftMostArg = mention.arguments.flatMap(xx => xx._2).map(m => m.start).min // first token index of all the arguments
+      action = mention.arguments("topic").head.asInstanceOf[EventMention] // should only be one
+      missedSubjs = action.sentenceObj.dependencies.get
+        // get the outgoing dep edges coming from the trigger of the action
+        .outgoingEdges(action.trigger.start)
+        // we're only interested in nsubj
+        .filter(tup => tup._2 == "nsubj")
+        // get the landing token (i.e., the subject of that action's token index)
+        .map(_._1)
+      // get the leftmost (or a dummy big number if there are none)
+      leftMostMissedSubj = if (missedSubjs.isEmpty) 1000 else missedSubjs.min
+      // check that trigger is to left of all args and any missed subjects
+      if triggerStart < leftMostArg && triggerStart < leftMostMissedSubj
+    } yield mention
+  }
+
+
+  def removeResearcher(
+      mentions: Seq[Mention],
+      state: State = new State()
+  ): Seq[Mention] = {
+    // If there are no timeintervals, return all events found
+    if (timeintervals._1.size == 0) {
+      return mentions
+    }
+    val to_be_returned = new ArrayBuffer[Mention]
+    for (men <- mentions) {
+      val startOffset = men match {
+        case cur: EventMention     => cur.trigger.startOffset
+        case cur: TextBoundMention => cur.startOffset
+      }
+
+      var cur = timeintervals._1(0)
+      var i = 0
+      while (cur < startOffset) {
+        i += 1
+        cur = timeintervals._1(i)
+      }
+      if (!(timeintervals._2 contains i)) {
+        to_be_returned.append(men)
+        if (timeintervals._3 contains timeintervals._1(i - 1)) {
+          // This checks if the previous utterance was a researcher question
+          //println(men.words.mkString)
+          // TODO create new event mention
+        }
+      }
+    }
+    to_be_returned
+  }
+
   def mkVictim(mentions: Seq[Mention], state: State = new State()): Seq[Mention] = {
     mentions.map(addArgLabel(_, TARGET_ARG, VICTIM, Some(ENTITY)))
   }
@@ -95,6 +158,12 @@ class TomcatActions() extends Actions with LazyLogging {
 }
 
 object TomcatActions {
-  def apply() = new TomcatActions()
+
+  def apply(
+      timeintervals: (ArrayBuffer[Int], ArrayBuffer[Int], ArrayBuffer[Int])
+  ) =
+    new TomcatActions(
+      timeintervals: (ArrayBuffer[Int], ArrayBuffer[Int], ArrayBuffer[Int])
+    )
 }
 
