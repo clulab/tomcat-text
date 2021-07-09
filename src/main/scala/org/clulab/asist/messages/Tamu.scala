c392621c2ee6ff91bcc773fa2e10e4146b78a9a6
Joseph Astier <jastier@gmail.com>
2021-07-09 16:30:29 -0700

    Added a data structure for comms to the TAMU server

---
+++ src/main/scala/org/clulab/asist/messages/Tamu.scala
@@ -0,0 +1,22 @@
+package org.clulab.asist.messages
+
+import org.json4s.jackson.Serialization.read
+
+/**
+ *  Authors:  Joseph Astier, Adarsh Pyarelal, Rebecca Sharp
+ *
+ *  Updated:  2021 July
+ *
+ *  TAMU messages
+ *
+ *  Messages for communication with the TAMU Dialog Act Classifier
+ */
+
+
+// outbound comms from DialogAgent client to TAMU server
+case class TamuDialogAgent(
+  participant_id: String = "",
+  text: String = "",
+  extractions:Seq[DialogAgentMessageDataExtraction] = Seq.empty
+)
+
