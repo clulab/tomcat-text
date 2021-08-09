2a974cfbb4ee31e061ec09b2933acd9274b5f0d6
Vincent Raymond <vraymond6@arizona.edu>
2021-08-09 16:06:04 -0700

    Removed unused scripts

--- annotations/scripts/run_label_studio.sh
+++
@@ -1,5 +0,0 @@
-sudo docker run -it -p 8080:8080 -v `pwd`/mydata:/label-studio/data \
---env LABEL_STUDIO_LOCAL_FILES_SERVING_ENABLED=true \
---env LABEL_STUDIO_LOCAL_FILES_DOCUMENT_ROOT=/label-studio/files \
--v "`pwd`/import_files:/label-studio/files" \
-heartexlabs/label-studio:latest label-studio
