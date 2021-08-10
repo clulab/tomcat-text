851e5d9e0f9d070fd77a41e7c95c73bbd3725470
Joseph Astier <jastier@gmail.com>
2021-08-10 15:14:18 -0700

    MQTT Agent DAC mods

--- src/main/scala/org/clulab/asist/agents/DialogAgentMqtt.scala
+++ src/main/scala/org/clulab/asist/agents/DialogAgentMqtt.scala
@@ -46,8 +46,8 @@ class DialogAgentMqtt(
 
   // this handles the message bus operations.  
   val bus = new MessageBusClient(
-    host, 
-    port, 
+    host,
+    port,
     subscriptions,
     publications,
     this
@@ -73,59 +73,74 @@ class DialogAgentMqtt(
 
   /** Receive a message from the message bus
    *  @param topic:  The message bus topic where the message was published
-   *  @param json:  A metadata text string
+   *  @param text:  A metadata text string, possibly multi-line
    */
   def messageArrived(
     topic: String,
-    json: String
+    text: String 
   ): Unit = {
-    json.split("\n").map{
+
+    // if the queue is empty, there is no aynchronous job in
+    // progress and it is safe to start a new one.
+    val noAsync = queue.isEmpty
+
+    // Each line of text is a discrete processing job ready to run
+    text.split("\n").map{
       line => queue.enqueue(BusMessage(topic, line))
     }
-    iteration
+
+    // start new async job if none are running
+    if(noAsync) nextJob
   }
 
 
-  // service the next input in line.
-  def iteration: Unit = {
-    // if the queue only contains the element we just added, there is no
-    // processing thread running.  Start one now.
-    if(queue.length == 1) {
-      processInput(queue.head)
-    }
+  // use the head of the queue as the next job.  Leave it in place
+  // until the job finishes.
+  def nextJob: Unit = if(!queue.isEmpty) processInput(queue.head)
 
-    // otherwise allow the returning processing thread to start the next.
+  // The job in process is the queue head.  When the job finishes, remove
+  // the queue head and start the next job.
+  def finishJob: Unit = {
+    queue.dequeue
+    nextJob
   }
-
 
   def processInput(input: BusMessage): Unit = input.topic match {
-    case `topicSubTrial` => trialMessageArrived(input)
-    case _ => dialogAgentMessageArrived (input)
+    case `topicSubTrial` => processTrialMessage(input)
+    case _ => processDialogAgentMessage (input)
   }
 
-  def dialogAgentMessageArrived(input: BusMessage): Unit = try {
+  def processDialogAgentMessage(input: BusMessage): Unit = try {
     val message = getDialogAgentMessage(
       source_type,
       input.topic,
       input.topic,
       read[Metadata](input.line)
     )
+    if(withClassifications) {
+      // start async threaded job
+    }
+    else {
+      val json = write(message)
+      bus.publish(topicPubDialogAgent, json)
+      finishJob
+    }
     // publish it
   } catch {
     case NonFatal(t) => logger.error("Could not parse {}", input.line)
   } 
 
   // send VersionInfo if we receive a TrialMessage with subtype "start", 
-  def trialMessageArrived(input: BusMessage): Unit = try {
+  def processTrialMessage(input: BusMessage): Unit = try {
     val tm = read[TrialMessage](input.line)
     if(tm.msg.sub_type == "start") {
       val timestamp = Clock.systemUTC.instant.toString
       val versionInfo = VersionInfo(this, timestamp)
-
-      // publish it
+      val json = write(versionInfo)
+      bus.publish(topicPubVersionInfo, json)
     }
+    finishJob
   } catch {
     case NonFatal(t) => logger.error("Could not parse {}", input.line)
   }
-
 }
