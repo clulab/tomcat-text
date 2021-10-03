c98d110c7595c5864a003bf1937ea221c57c03ee
remo-help <1163925remo@gmail.com>
2021-10-02 19:09:48 -0700

    fixed a broken test

--- src/test/scala/org/clulab/asist/rules/TestQuestions.scala
+++ src/test/scala/org/clulab/asist/rules/TestQuestions.scala
@@ -60,12 +60,22 @@ class TestQuestions extends BaseTest {
     val that = DesiredMention("DemPron", "that")
     val there = DesiredMention("Deictic", "there")
     val plan = DesiredMention("Plan", "plan")  // fixme: one day these should be a proper label in the taxonomy
-    val q1 = DesiredMention("Question", "What's that over there", Map("topic" -> Seq(that), "location" -> Seq(there)))
+    //val q1 = DesiredMention("Question", "What's that over there", Map("topic" -> Seq(that), "location" -> Seq(there)))
     val q2 = DesiredMention("Question", "What is the plan", Map("topic" -> Seq(plan)))
     val q3 = DesiredMention("Question", "What is that", Map("topic" -> Seq(that)))
-    testMention(mentions, q1)
+    //testMention(mentions, q1)
     testMention(mentions, q2)
     testMention(mentions, q3)
+  }
+
+  it should "handle contracted information gathering questions" in {
+    val text = "What's that over there?"
+    val mentions = extractor.extractFrom(text)
+    val that = DesiredMention("DemPron", "that")
+    val there = DesiredMention("Deictic", "there")
+    val quest1 = DesiredMention("Question", "What's that over there", Map("topic" -> Seq(that), "location" ->Seq(there)))
+
+    testMention(mentions,quest1)
   }
 
   it should "handle Yes_No questions" in {
