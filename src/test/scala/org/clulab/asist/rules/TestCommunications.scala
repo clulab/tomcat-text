0c0c996dbc97f051b4ac5dc169231a8813be8ad7
YuweiSophieWang <60522232+Yuweien@users.noreply.github.com>
2021-11-10 00:19:46 -0700

    Add tests for HelpRequest and ToolBroken lables

--- src/test/scala/org/clulab/asist/rules/TestCommunications.scala
+++ src/test/scala/org/clulab/asist/rules/TestCommunications.scala
@@ -119,4 +119,38 @@ class TestCommunications extends BaseTest {
 
     testMention(mentions, inst)
   }
+
+
+  passingTest should "parse simple help request" in {
+    val text = "I need help."
+    val mentions = extractor.extractFrom(text)
+    val helpReq = DesiredMention("HelpRequest", "need help",Map.empty,Set(AGENT_SELF))
+
+    testMention(mentions, helpReq)
+  }
+
+  passingTest should "parse complex help request" in {
+    val text = "Can you help me here?"
+    val mentions = extractor.extractFrom(text)
+    val helpReq = DesiredMention("HelpRequest", "you help me here",
+      Map("helper" -> Seq(DesiredMention("You", "you")),
+        "location" -> Seq(DesiredMention("Deictic", "here"))
+      ),
+      Set(AGENT_SELF))
+
+    testMention(mentions, helpReq)
+  }
+
+  passingTest should "parse tool broken" in {
+    val text = "The hammer is broken."
+    val mentions = extractor.extractFrom(text)
+    val toolBroken = DesiredMention("ToolBroken", "hammer is broken",
+      Map("tool" -> Seq(DesiredMention("Hammer", "hammer"))))
+
+    testMention(mentions, toolBroken)
+  }
+
+
+
+
 }
