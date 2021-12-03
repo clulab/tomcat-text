fe2191c4431e6d13fac08e33ec9427d1b10291d8
Joseph Astier <jastier@gmail.com>
2021-12-03 09:09:55 -0700

    Added a heartbeat subclass

--- src/main/scala/org/clulab/asist/agents/MqttAgent.scala
+++ src/main/scala/org/clulab/asist/agents/MqttAgent.scala
@@ -1,10 +1,12 @@
 package org.clulab.asist.agents
 
+import ai.lum.common.ConfigFactory
 import org.clulab.utils.{MessageBusClient, MessageBusClientListener}
 import org.clulab.asist.messages._
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 import buildinfo.BuildInfo
+
 
 /**
  * Authors:  Joseph Astier, Adarsh Pyarelal
@@ -33,11 +35,12 @@ class DialogAgentMqttCoordinator(
   )
 }
 
-
 abstract class MqttAgentBase(
   val mqttHost: String = "",
   val mqttPort: String = ""
 ) extends LazyLogging with MessageBusClientListener {
+
+  val config: Config = ConfigFactory.load()
 
   // A single Message Bus message
   case class BusMessage (
@@ -52,15 +55,35 @@ abstract class MqttAgentBase(
   logger.info(s"Publishing on ${pubTopic}")
 }
 
-/** support production of heartbeat and VersionInfo messages */
+/** support production of VersionInfo messages */
+class MqttAgentHeartbeat(
+  override val mqttHost: String = "",
+  override val mqttPort: String = ""
+) extends MqttAgentBase {
+
+  override def subTopic = "TrialSubTopic"
+  override def pubTopic = config.getString("DialogAgent.heartbeatTopic")
+
+  /** Receive a message from the message bus
+   * @param topic:  The message bus topic where the message was published
+   * @param text:  A metadata text string, possibly multi-line
+   */
+  def messageArrived(
+    topic: String,
+    text: String
+  ): Unit = {
+    logger.info("messageArrived")
+  }
+}
+
+/** support production of VersionInfo messages */
 class MqttAgentTrial(
   override val mqttHost: String = "",
   override val mqttPort: String = ""
 ) extends MqttAgentBase {
 
   override def subTopic = "TrialSubTopic"
-  override def pubTopic = "TrialPubTopic"
-
+  override def pubTopic = config.getString("DialogAgent.versionInfoTopic")
 
   /** Receive a message from the message bus
    * @param topic:  The message bus topic where the message was published
@@ -80,7 +103,7 @@ class MqttAgentChat(
 ) extends MqttAgentBase {
 
   override def subTopic = "ChatSubTopic"
-  override def pubTopic = "ChatPubTopic"
+  override def pubTopic = config.getString("DialogAgent.outputTopic")
 
   /** Receive a message from the message bus
    * @param topic:  The message bus topic where the message was published
@@ -100,7 +123,7 @@ class MqttAgentSpeech(
 ) extends MqttAgentBase {
 
   override def subTopic = "SpeechSubTopic"
-  override def pubTopic = "SpeechPubTopic"
+  override def pubTopic = config.getString("DialogAgent.outputTopic")
 
   /** Receive a message from the message bus
    * @param topic:  The message bus topic where the message was published
@@ -121,7 +144,7 @@ class MqttAgentIdc(
 ) extends MqttAgentBase {
 
   override def subTopic = "IdcSubTopic"
-  override def pubTopic = "IdcPubTopic"
+  override def pubTopic = config.getString("DialogAgent.outputTopic")
 
   /** Receive a message from the message bus
    * @param topic:  The message bus topic where the message was published
