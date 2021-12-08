311aea615507c9a099404f32b9542e601160a12c
Joseph Astier <jastier@gmail.com>
2021-12-08 09:22:06 -0700

    Added a configuration method for trial input

--- src/main/scala/org/clulab/asist/messages/HeartbeatMessage.scala
+++ src/main/scala/org/clulab/asist/messages/HeartbeatMessage.scala
@@ -1,5 +1,9 @@
 package org.clulab.asist.messages
 
+import buildinfo.BuildInfo
+import com.typesafe.config.Config
+
+
 /**
  *  Authors:  Joseph Astier, Adarsh Pyarelal
  *
@@ -13,7 +17,7 @@ package org.clulab.asist.messages
  *   "header": {
  *     "timestamp": "2021-10-16T01:07:52.824Z",
  *     "message_type": "status",
- *     "version": "0.1"    // current testbed version
+ *     "version": "0.1" // use testbed trial version if available
  *   },
  *   "msg": {
  *     "timestamp": "2021-10-16T01:07:52.824Z",
@@ -33,14 +37,41 @@ package org.clulab.asist.messages
  */
 
 case class HeartbeatMessageData(
-  state: String = "OK",
-  active: String = "true",
-  status: String = "I am processing messages"
+  state: String = null,
+  active: String = null,
+  status: String = null
 )
 
 /** Contains the complete specification for a Heartbeat message */
 case class HeartbeatMessage (
-  header: CommonHeader = new CommonHeader(message_type = "status"),
-  msg: CommonMsg = new CommonMsg(sub_type = "heartbeat"),
-  data: HeartbeatMessageData = new HeartbeatMessageData
+  header: CommonHeader,
+  msg: CommonMsg,
+  data: HeartbeatMessageData
 ) 
+
+object HeartbeatMessage {
+
+  def apply(
+    config: Config, 
+    trialMessage: TrialMessage
+  ): HeartbeatMessage = HeartbeatMessage(
+    CommonHeader(
+      // timestamp set when published
+      message_type = config.getString("Heartbeat.header.message_type"),
+      version = trialMessage.header.version
+    ),
+    CommonMsg(
+      // timestamp set when published
+      source = config.getString("Heartbeat.msg.source"),
+      sub_type = config.getString("Heartbeat.msg.sub_type"),
+      version = BuildInfo.version,
+      trial_id = trialMessage.msg.trial_id,
+      experiment_id = trialMessage.msg.experiment_id
+    ),
+    HeartbeatMessageData(
+      state = config.getString("Heartbeat.data.state"),
+      active = config.getString("Heartbeat.data.active"),
+      status = config.getString("Heartbeat.data.status")
+    )
+  )
+}
