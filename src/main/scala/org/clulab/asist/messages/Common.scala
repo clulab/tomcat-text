2c89035f819e772092d2d1544d8fd8ef1189e3af
Joseph Astier <jastier@gmail.com>
2022-01-06 16:19:51 -0700

    Added BusMessage case class, fixed comments

--- src/main/scala/org/clulab/asist/messages/Common.scala
+++ src/main/scala/org/clulab/asist/messages/Common.scala
@@ -6,10 +6,14 @@ package org.clulab.asist.messages
  *  Components used by more than one message class.
  */
 
+case class Topic(
+  topic: String = "N/A"
+)
+
 case class CommonHeader(
   timestamp: String = "N/A", // "2019-12-26T12:47:23.1234Z"
-  message_type: String = "N/A", // "agent"
-  version: String = "N/A", // trial version or 0.1 if trial not available
+  message_type: String = "N/A", // "event"
+  version: String = "N/A", // trial header version or 0.1 if not available
 )
 
 // This subset of the testbed CommonMsg struct contains only fields we use
@@ -17,9 +21,15 @@ case class CommonMsg (
   experiment_id: String = "N/A", // "123e4567-e89b-12d3-a456-426655440000"
   trial_id: String = "N/A", //  "123e4567-e89b-12d3-a456-426655440000"
   timestamp: String = "N/A", // "2019-12-26T14:05:02.1412Z"
-  source: String = "N/A", // "uaz_dialog_agent",
-  sub_type: String = "N/A", // "event"
-  version: String = "N/A", // "3.1.4"
+  source: String = "N/A", // "message_bus",
+  sub_type: String = "N/A", // "Event:dialogue_event"
+  version: String = "N/A", // "4.0.0"
   replay_root_id: String = "N/A", // "123e4567-e89b-12d3-a456-426655440000"
   replay_id: String = "N/A" // "876e4567-ab65-cfe7-b208-426305dc1234"
+)
+
+// A single Message Bus message
+case class BusMessage(
+  topic: String = "N/A",
+  text: String = "N/A" // may contain newlines
 )
