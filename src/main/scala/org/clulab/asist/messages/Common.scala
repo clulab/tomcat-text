ed56f9e156aae50a76eb8842d1d5a2a93cc93c8b
jastier <jastier@users.noreply.github.com>
2022-01-11 19:24:01 +0000

    deploy: ce9749a52ba9ddd5f12d6121fc3c464fa56012b2

--- src/main/scala/org/clulab/asist/messages/Common.scala
+++ src/main/scala/org/clulab/asist/messages/Common.scala
@@ -4,6 +4,8 @@ package org.clulab.asist.messages
  *  Authors:  Joseph Astier, Adarsh Pyarelal, Rebecca Sharp
  *
  *  Components used for subscription and publication on the Message Bus
+ *
+ *  Optional fields are omitted from output if value is not known
  */
 
 case class Topic(
@@ -11,19 +13,19 @@ case class Topic(
 )
 
 case class CommonHeader(
-  timestamp: String = "N/A", // "2019-12-26T12:47:23.1234Z"
-  message_type: String = "N/A", // "event"
-  version: String = "N/A", // trial header version or 0.1 if not available
+  timestamp: String = "N/A",
+  message_type: String = "N/A",
+  version: String = "N/A",
 )
 
 // This subset of the testbed CommonMsg struct contains only fields we use
 case class CommonMsg (
-  experiment_id: String = "N/A", // "123e4567-e89b-12d3-a456-426655440000"
-  trial_id: String = "N/A", //  "123e4567-e89b-12d3-a456-426655440000"
-  timestamp: String = "N/A", // "2019-12-26T14:05:02.1412Z"
-  source: String = "N/A", // "message_bus",
-  sub_type: String = "N/A", // "Event:dialogue_event"
-  version: String = "N/A", // "4.0.0"
-  replay_root_id: String = "N/A", // "123e4567-e89b-12d3-a456-426655440000"
-  replay_id: String = "N/A" // "876e4567-ab65-cfe7-b208-426305dc1234"
+  experiment_id: String = "N/A",
+  trial_id: String = "N/A",
+  timestamp: String = "N/A",
+  source: String = "N/A",
+  sub_type: String = "N/A",
+  version: String = "N/A",
+  replay_root_id: String = "N/A", // omitted if null
+  replay_id: String = "N/A"  // omitted if null
 )
